4efecb951480603028236eec7ff45ea3b424f4e5
zandra <126991568+zandra-handz@users.noreply.github.com>
2024-02-14 20:57:31 -0500

    Update settings.py

--- root/root/settings.py
+++ root/root/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-ulk_hius7lv6hv#tm72-+(%^^@0xtz1ze=pzxy$9yil*fo%=!-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['climatetwin-lzyyd.ondigitalocean.app']
 
 
 # Application definition
