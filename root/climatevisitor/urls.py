fdcde72992f7708c1a2354ef678af43abcd2ffd5
zandra-handz <taylorcolegrove@gmail.com>
2024-02-17 16:35:56 -0500

    making user id retrieval more consistent across views

--- root/climatevisitor/urls.py
+++ root/climatevisitor/urls.py
@@ -6,6 +6,8 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('', views.index, name='index'),
     path('endpoints/', views.endpoints, name='endpoints'),
+
+    # Play the game
     path('go/', views.go, name='go'),
     path('currently-visiting/', views.CurrentTwinLocationView.as_view(), name='currently-visiting-location'),
     path('currently-nearby/', views.CurrentDiscoveryLocationsView.as_view(), name='current-nearby-locations'),
@@ -13,12 +15,16 @@ urlpatterns = [
     path('currently-exploring/', views.CurrentExploreLocationView.as_view(), name='currently-exploring'),
     path('collect/', views.collect, name='collect-treasure'),
     path('item-choices/', views.item_choices, name="item-choices"),
+
+    # Access all data associated with user
     path('locations/twins', views.TwinLocationsView.as_view(), name='twin-locations'),
     path('locations/twin/<int:pk>', views.TwinLocationView.as_view(), name='twin-location'),
     path('locations/nearby/', views.DiscoveryLocationsView.as_view(), name='ancient-ruins-found'),
     path('locations/nearby<int:pk>', views.DiscoveryLocationView.as_view(), {'delete': 'destroy'}, name='delete-discovery-location'),
     path('locations/explore/', views.ExploreLocationsView.as_view(), name='explored-locations'),
     path('explore-location/<int:pk>', views.ExploreLocationView.as_view(), name='explored-location'),
+
     path('api-token-auth/', obtain_auth_token)
 ]
+ 
  
