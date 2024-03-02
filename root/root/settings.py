2c326b94b1d9bfa007368996fb814c5db364f4b2
zandra-handz <taylorcolegrove@gmail.com>
2024-03-01 19:00:58 -0500

    changed REDIS_URL

--- root/root/settings.py
+++ root/root/settings.py
@@ -90,7 +90,7 @@ ASGI_APPLICATION = 'root.asgi.application'
 #local:
 #CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
 
-REDIS_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379')
+REDIS_URL = os.environ.get('REDIS_URL')
 
 
 
