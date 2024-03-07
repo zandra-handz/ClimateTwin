b9c81db33d2c91e1e97656406a472242fcb22392
zandra-handz <taylorcolegrove@gmail.com>
2024-03-06 22:01:13 -0500

    added a third websocket for current twin location updates

--- root/staticfiles/climatevisitor/js/coordinatesloader.js
+++ root/staticfiles/climatevisitor/js/coordinatesloader.js
@@ -1,6 +1,5 @@
 // coordinatesloader.js
 
-
 function displayLoading(resultContainerId) {
     var resultContainer = document.getElementById(resultContainerId);
     resultContainer.innerHTML = '';
@@ -68,24 +67,84 @@ function replaceUpdate(update) {
         // Display completed message if no coordinates left
         updateElement.textContent = 'Completed';
         updateElement.style.color = 'gray';
-
-        // Fade out and remove the message after 5 seconds
-        const fadeOutDuration = 2000; // 2 seconds
-        const fadeOutInterval = 20; // 20 milliseconds
-        const initialOpacity = 1;
-
-        let opacity = initialOpacity;
-        const intervalId = setInterval(() => {
-            opacity -= (1 / (fadeOutDuration / fadeOutInterval));
-            updateElement.style.opacity = opacity;
-            if (opacity <= 0) {
-                clearInterval(intervalId);
-                updateElement.remove();
-            }
-        }, fadeOutInterval);
     }
 
     container.innerHTML = '';  
     container.appendChild(updateElement);
 }
 
+
+// coordinatesloader.js
+
+function displayLocationUpdate(resultContainerId) {
+    var resultContainer = document.getElementById(resultContainerId);
+    resultContainer.innerHTML = '';
+    var loadingDiv = document.createElement('div');
+    loadingDiv.classList.add('loading-container');
+    //loadingDiv.innerHTML = '<div class="spinner"></div>';
+    resultContainer.appendChild(loadingDiv);
+
+    // WebSocket connection
+    //const socket = new WebSocket('wss://localhost:8000/ws/climate-twin/'); // Replace with your WebSocket URL
+    const socket = new WebSocket('wss://climatetwin-lzyyd.ondigitalocean.app/ws/climate-twin/');
+
+    // Event listener -- open
+    socket.onopen = function(event) {
+        console.log('WebSocket connection opened');
+    };
+
+    // Event listener -- messages
+    socket.onmessage = function(event) {
+        const update = JSON.parse(event.data);
+        locationUpdate(update); 
+    };
+
+    // Event listener -- close
+    socket.onclose = function(event) {
+        console.log('WebSocket connection closed');
+    };
+
+    // Event listener -- errors
+    socket.onerror = function(error) {
+        console.error('WebSocket error:', error);
+    };
+}
+
+// Function to append new update to the container
+function locationUpdate(update) {
+    const container = document.getElementById('location-update-container');
+    const updateElement = document.createElement('div');
+    
+    // Apply CSS styles to center, make it smaller, and add padding
+    updateElement.style.textAlign = 'center';
+    updateElement.style.fontSize = 'small';
+    updateElement.style.paddingBottom = '32px'; // Adjust the value as needed
+ 
+    if (update.latitude !== undefined && update.longitude !== undefined) { 
+        updateElement.textContent = `Success! You are now in ${update.name}, Lat: ${update.latitude}, Long: $'{update.longitude}`; // ${update.latitude}, ${update.longitude}`;
+        updateElement.style.color = 'black'; 
+ 
+        if (update.latitude > 0) {
+            updateElement.innerHTML += `<br>Lat: <span style="color: green">${update.latitude}</span>`;
+        } else if (update.latitude < 0) {
+            updateElement.innerHTML += `<br>Lat: <span style="color: red">${update.latitude}</span>`;
+        } else {
+            updateElement.innerHTML += `<br>Lat: ${update.latitude}`;
+        }
+
+        if (update.longitude > 0) {
+            updateElement.innerHTML += `, Long: <span style="color: blue">${update.longitude}</span>`;
+        } else if (update.longitude < 0) {
+            updateElement.innerHTML += `, Long: <span style="color: orange">${update.longitude}</span>`;
+        } else {
+            updateElement.innerHTML += `, Long: ${update.longitude}`;
+        }
+    } else {
+        // Display completed message if no coordinates left
+        updateElement.textContent = 'Completed';
+        updateElement.style.color = 'gray';
+    }
+
+    container.innerHTML = '';  
+    container.appendChild(updateElement);
+}
