c5e3ac9801353b457cc0234c73c9329121855a49
zandra-handz <taylorcolegrove@gmail.com>
2024-03-07 17:55:52 -0500

    fixed html for third web socket more

--- root/staticfiles/climatevisitor/js/coordinatesloader.js
+++ root/staticfiles/climatevisitor/js/coordinatesloader.js
@@ -121,7 +121,7 @@ function locationUpdate(update) {
     updateElement.style.paddingBottom = '32px'; // Adjust the value as needed
  
     if (update.latitude !== undefined && update.longitude !== undefined) { 
-        updateElement.textContent = `Success! You are now in ${update.name}, Lat: ${update.latitude}, Long: $'{update.longitude}`; // ${update.latitude}, ${update.longitude}`;
+        updateElement.textContent = `Success! You are now in ${update.name}, Lat: ${update.latitude}, Long: ${update.longitude}`; // ${update.latitude}, ${update.longitude}`;
         updateElement.style.color = 'black'; 
  
         if (update.latitude > 0) {
