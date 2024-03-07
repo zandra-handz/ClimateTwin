fb5ed6ef58ecd769bfeaa0aa117d8b3d7474edc0
zandra-handz <taylorcolegrove@gmail.com>
2024-03-06 20:27:46 -0500

    added country name output to websocket from twin finder algorithm

--- root/climatevisitor/tasks/tasks.py
+++ root/climatevisitor/tasks/tasks.py
@@ -8,7 +8,7 @@ from ..consumer import ClimateTwinConsumer
 
 # Name currently inaccurate; this is getting processed by main server
 @shared_task
-def send_coordinate_update_to_celery(latitude, longitude):
+def send_coordinate_update_to_celery(country_name, latitude, longitude):
     # Call the function to update animation with the given coordinates
     update_animation(latitude, longitude)
     print("Sent coords to animation")
@@ -18,11 +18,12 @@ def send_coordinate_update_to_celery(latitude, longitude):
         'climate_updates',  # Name of the group your consumer is listening to
         {
             'type': 'update_coordinates',  # Name of the consumer method to call
+            'country_name': country_name,
             'latitude': latitude,
             'longitude': longitude,
         }
     )
-    print(f"Sending update for coordinate pair: {latitude}, {longitude}")
+    print(f"Sending Twin Finder location update: {country_name}, {latitude}, {longitude}")
 
     # Call the consumer method directly for testing purposes
 
