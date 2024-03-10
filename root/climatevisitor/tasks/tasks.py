6f2cbbb90319d68f6deec28805447fae2c4c2d1d
zandra-handz <taylorcolegrove@gmail.com>
2024-03-10 19:58:54 -0400

    id passing

--- root/climatevisitor/tasks/tasks.py
+++ root/climatevisitor/tasks/tasks.py
@@ -5,7 +5,9 @@ from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 from ..animations import update_animation
 
+
 # Name currently inaccurate; this is getting processed by main server
+'''
 @shared_task
 def send_coordinate_update_to_celery(country_name, latitude, longitude):
     # Call the function to update animation with the given coordinates
@@ -22,6 +24,31 @@ def send_coordinate_update_to_celery(country_name, latitude, longitude):
         }
     )
     print(f"Sending Twin Finder location update: {country_name}, {latitude}, {longitude}")
+'''
+
+# Testing passing in user
+
+@shared_task
+def send_coordinate_update_to_celery(user_id, country_name, latitude, longitude):
+    # Call the function to update animation with the given coordinates
+    update_animation(latitude, longitude)
+    print("Sent coords to animation")
+    channel_layer = get_channel_layer()
+    
+    # Construct the group name using the user ID
+    group_name = f'climate_updates_{user_id}'
+    
+    async_to_sync(channel_layer.group_send)(
+        group_name,  # Name of the group associated with the user
+        {
+            'type': 'update_coordinates',  # Name of the consumer method to call
+            'country_name': country_name,
+            'latitude': latitude,
+            'longitude': longitude,
+        }
+    )
+    print(f"Sending Twin Finder location update: {country_name}, {latitude}, {longitude}")
+
 
     # Call the consumer method directly for testing purposes
 
