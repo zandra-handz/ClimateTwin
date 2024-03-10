065b4b55f886634c705ec93a71468df79c4230e8
zandra-handz <taylorcolegrove@gmail.com>
2024-03-10 12:38:07 -0400

    changed web socket wrap from AuthMiddlewareStack to TokenAuthMiddlewareStack

--- root/root/asgi.py
+++ root/root/asgi.py
@@ -6,13 +6,14 @@ from channels.auth import AuthMiddlewareStack
 from django.urls import path
 from climatevisitor.consumers import AnimationConsumer
 from climatevisitor.routing import websocket_urlpatterns # Correct import
+from root.middleware import TokenAuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'root.settings')
 
-# Get the Django ASGI application
+
 django_asgi_app = get_asgi_application()
 
-# Define a logger
+
 logger = logging.getLogger(__name__)
 
 # Define a function to log request headers
@@ -23,8 +24,12 @@ async def log_request_headers(scope, receive, send):
 
 # Define the ProtocolTypeRouter for ASGI
 application = ProtocolTypeRouter({
-    "http": log_request_headers,
-    "websocket": AuthMiddlewareStack(
-        URLRouter(websocket_urlpatterns)  # Correct usage of routing
+    #"http": log_request_headers,
+    #"websocket": AuthMiddlewareStack(
+    #    URLRouter(websocket_urlpatterns)  # Correct usage of routing
+   # ),
+#})
+    'websocket': TokenAuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)
     ),
 })
