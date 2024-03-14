0acd0b1d01c8fb352f2fd929e345764c5870c2b5
zandra-handz <taylorcolegrove@gmail.com>
2024-03-13 21:50:40 -0400

    commented out log info in consumer.py file and coordinateloader.js file

--- root/climatevisitor/tasks/tasks.py
+++ root/climatevisitor/tasks/tasks.py
@@ -7,23 +7,19 @@ from ..animations import update_animation
 
 
 # Name currently inaccurate; this is getting processed by main server
-
 # Testing passing in user
 
 @shared_task
 def send_coordinate_update_to_celery(user_id, country_name, temperature, latitude, longitude):
-    # Call the function to update animation with the given coordinates
-    # update_animation(latitude, longitude)
-    # print("Sent coords to animation")
+
     channel_layer = get_channel_layer()
-    
-    # Construct the group name using the user ID
+     
     group_name = f'climate_updates_{user_id}'
     
     async_to_sync(channel_layer.group_send)(
-        group_name,  # Name of the group associated with the user
+        group_name,  
         {
-            'type': 'update_coordinates',  # Name of the consumer method to call
+            'type': 'update_coordinates',  
             'country_name': country_name,
             'temperature': temperature,
             'latitude': latitude,
@@ -32,15 +28,12 @@ def send_coordinate_update_to_celery(user_id, country_name, temperature, latitud
     )
     print(f"Sending Twin Finder location update: {country_name}, {temperature} degrees F, {latitude}, {longitude}")
 
-
-    # Call the consumer method directly for testing purposes
-
+ 
 @shared_task
 def send_location_update_to_celery(user_id, name, temperature, latitude, longitude):
 
     channel_layer = get_channel_layer()
     
-    # Construct the group name using the user ID
     group_name = f'location_update_{user_id}'
     
     async_to_sync(channel_layer.group_send)(
