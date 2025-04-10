da954110d6b3bedcb53ba109c0354e87d57c38bb
zandra_handz <taylorcolegrove@gmail.com>
2025-04-10 14:03:09 -0400

    both places

--- root/climatevisitor/tasks/algorithms_task.py
+++ root/climatevisitor/tasks/algorithms_task.py
@@ -289,13 +289,14 @@ def process_climate_twin_request(self, user_id, user_address):
     except Exception as exc:
         logger.error(f"Error processing climate twin request: {exc}. Retrying...")
         retry_exc = exc
-    finally:
-        remove_twin_search_lock(user_id)
-        logger.info(f"Deleted lock for user {user_id}")
+    # finally:
+    #     remove_twin_search_lock(user_id)
+    #     logger.info(f"Deleted lock for user {user_id}")
 
     if retry_exc:
         raise self.retry(exc=retry_exc)
 
+    remove_twin_search_lock(user_id)
     logger.info("Task to process climate twin request completed.")
     return "Request sent for processing"
 
